978a496db3222e315e4d510f742725db7ea070c6
BlaiseMohamed <blaisemohamed@outlook.fr>
2024-10-27 09:49:26 +0100

    add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,49 @@
 <template>
+    <header>
+        <nav>
+            <h1>
+                Mon Portfolio
+            </h1>
+            <menu>
+                <li><a href="#presentation">Pésentation</a></li>
+                <li><a href="#Projet"> Projets</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </menu>
+        </nav>
+    </header>
 
+    <main>
+        <section id="presentation">
+
+            <h2>
+                À propos de moi !
+            </h2>
+            <p>
+                Salut ! Moi, c'est Mohamed Blaise je suis étudiant , ardent à l'idée de plonger dans 
+                l'univers du codage, du développement web (front-end et back-end avec HTML, CSS, JavaScript, React, Node.js, et Python) ainsi que dans l'univers de 
+                l'intelligence artificielle. Je souhaite travailler dans ce domaine pour devenir développeur web, maîtriser ces technologies et explorer tout le potentiel de l'informatique.
+            </p>
+
+        </section>
+
+        <section id="Projet">
+            <h2>Mes Projets</h2>
+        <h3>
+            Mon CV en ligne !
+        </h3>
+        <h3>
+            Le cahier des charges
+        </h3>
+        <h3>
+            Dynamiser un espace commentaires
+        </h3>
+        </section>
+
+        <section id="contact">
+        </section>
+    </main>
+
+    <footer></footer>
 </template>
 
 
